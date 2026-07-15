1fc2c81d32fe924fbcb1f414f17e708472d4cdb9
stephengeorge <txsteven@gmail.com>
2026-07-15 10:27:41 -0500

    adjusted playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,14 +33,6 @@ export default defineConfig({
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-    {
-      name: 'mobile-chrome',
-      use: { ...devices['Pixel 7'] },
-    },
+    }
   ],
 });
